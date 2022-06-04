c1e7470917a643d46d477c7e29ddc695e1a6cd88
Evyde <life.app.hanfeng@hotmail.com>
2022-06-05 00:12:56 +0800

    🪟 feat(Client/MainFrame): 新增 主窗体大概框架

--- Client/src/main/java/jlu/evyde/gobang/Client/SwingView/MainFrame.java
+++ Client/src/main/java/jlu/evyde/gobang/Client/SwingView/MainFrame.java
@@ -1,34 +1,99 @@
 package jlu.evyde.gobang.Client.SwingView;
-import jlu.evyde.gobang.Client.Controller.Utils;
+import jlu.evyde.gobang.Client.SwingController.GUIPrintStream;
 
 import javax.swing.*;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.io.PrintStream;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-
 public class MainFrame extends JFrame {
-    private JLabel userName;
-    private final ResourceBundle bundle = ResourceBundle.getBundle("MainFrame", new Locale("zh_CN"));
+    private JTextPane logTextPane;
+    private final ResourceBundle bundle;
     public MainFrame() {
         super();
-        this.setTitle(Utils.toUTF8String(bundle.getString("title")));
+
         Locale locale = Locale.getDefault();
+        bundle = ResourceBundle.getBundle("MainFrame", locale);
+
+        initComponents();
+
+        PrintStream stdout = new GUIPrintStream(System.out, logTextPane);
+        System.setOut(stdout);
+        PrintStream stderr = new GUIPrintStream(System.err, logTextPane);
+        System.setErr(stderr);
+    }
+
+    private void initComponents() {
+        this.setTitle(bundle.getString("title"));
+        this.setSize(new Dimension(1024, 768));
+        this.setPreferredSize(new Dimension(1024, 768));
 
         JFrame.setDefaultLookAndFeelDecorated(true);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout(1, 3));
-        userName = new JLabel();
+        GridBagLayout mainGridBagLayout = new GridBagLayout();
 
-        this.add(userName);
-        this.add(new JTextField());
-        this.add(new JLabel());
-        this.add(new JPasswordField());
-        this.add(new JButton(Utils.toUTF8String(bundle.getString("login"))));
+        mainGridBagLayout.columnWidths = new int[] { 0 };
+        mainGridBagLayout.rowHeights = new int[] { 0 };
+        mainGridBagLayout.columnWeights = new double[] { 1.0 };
+        mainGridBagLayout.rowWeights = new double[] { 1.0 };
+
+        this.setLayout(mainGridBagLayout);
+
+        GridBagConstraints s = new GridBagConstraints();
+
+        JPanel controlPanel = new JPanel(new GridBagLayout());
+        controlPanel.setBorder(BorderFactory.createTitledBorder(bundle.getString("controlPanel")));
+
+        logTextPane = new JTextPane();
+
+        JScrollPane logScrollPane = new JScrollPane(
+                logTextPane,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+        );
+
+        logScrollPane.setPreferredSize(new Dimension(100, 50));
+        logScrollPane.setBorder(BorderFactory.createTitledBorder(bundle.getString("log")));
+        s.gridx = 0;
+        s.gridy = 3;
+        s.gridheight = 4;
+        s.fill = GridBagConstraints.BOTH;
+        s.weightx = 1;
+        s.weighty = 4;
+        controlPanel.add(logScrollPane, s);
+
+        s = new GridBagConstraints();
+        s.gridx = 0;
+        s.gridy = 0;
+        s.gridheight = 1;
+        s.gridwidth = 1;
+        s.ipadx = -50;
+        s.ipady = -50;
+        s.weightx = 1;
+        s.weighty = 1;
+        s.fill = GridBagConstraints.BOTH;
+        this.add(controlPanel, s);
+
+
+        JPanel gamePanel = new JPanel();
+        gamePanel.setBorder(BorderFactory.createTitledBorder(bundle.getString("gamePanel")));
+
+        s = new GridBagConstraints();
+        s.gridx = 1;
+        s.gridy = 0;
+        s.gridwidth = 2;
+        s.fill = GridBagConstraints.BOTH;
+        s.ipadx = -50;
+        s.ipady = -50;
+        s.weightx = 2;
+        s.weighty = 1;
+        this.add(gamePanel, s);
+
+
+
+
         this.pack();
     }
 
