e8fee90500b478a50d6dd404ab7daf3503b388ef
Evyde <life.app.hanfeng@hotmail.com>
2022-06-05 00:12:10 +0800

    🔧 fix(Client/Main): 修复 移除日志测试代码

--- Client/src/main/java/jlu/evyde/gobang/Client/Main.java
+++ Client/src/main/java/jlu/evyde/gobang/Client/Main.java
@@ -10,12 +10,14 @@ import jlu.evyde.gobang.Client.Model.SystemConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
     /**{
      * 创建并显示GUI。出于线程安全的考虑，
      * 这个方法在事件调用线程中调用。
      */
-    private static final Logger logger = LoggerFactory.getLogger(SystemConfiguration.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         logger.info("Starting client");
@@ -27,6 +29,20 @@ public class Main {
             logger.error("MainFrame started failed: {}", ffe.toString());
             System.exit(1);
         }
+
+//        while (true) {
+//            int randomChoice = (int) (Math.random() * 10 + 1);
+//            if (randomChoice < 5) {
+//                logger.error("ERROR");
+//            } else {
+//                logger.info("INFO");
+//            }
+//            try {
+//                sleep(300);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
     }
 }
