92594dd41c6a8a3911284786986940f26c1bc47b
Evyde <life.app.hanfeng@hotmail.com>
2022-06-05 19:53:13 +0800

    ❌ feat(Client/GobangException): 新增 几个异常

--- Client/src/main/java/jlu/evyde/gobang/Client/Controller/GobangException.java
+++ Client/src/main/java/jlu/evyde/gobang/Client/Controller/GobangException.java
@@ -3,4 +3,6 @@ package jlu.evyde.gobang.Client.Controller;
 public class GobangException {
     public static class FrameInitFailedException extends RuntimeException {}
     public static class UICommunicatorInitFailedException extends RuntimeException {}
+    public static class MQServerStartFailedException extends RuntimeException {}
+    public static class MQServerClosedFailedException extends RuntimeException {}
 }
